8b5f3f62ae987b738eda104e782a5a572c646e62
Jale93 <memmedli.jale93@gmail.com>
2020-10-30 05:49:06 +0000

    Update AmazonTest.java Macbook Air

--- ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
+++ ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
@@ -37,7 +37,7 @@ CommonDriver cmnDriver;
 	
 	@Test
 	public void searchProduct() throws Exception{
-		String product = "Apple watch";
+		String product = "Apple MacbookAir";
 		String category = "Electronics";
 		
 		homepage.searchProduct(product, category);
